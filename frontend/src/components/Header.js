202b5ba83fccceb69efc805b7c3a8b6f990cb2b4
Moran <moran@MBP00723-GENERAL.local>
2022-05-30 16:46:46 +0300

    seed12901

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -24,7 +24,7 @@ const LoggedOutView = (props) => {
 };
 
 const LoggedInView = (props) => {
-  console.log("heyy")
+  console.log("heyyy")
   if (props.currentUser) {
     return (
       <ul className="navbar-nav ml-auto">
